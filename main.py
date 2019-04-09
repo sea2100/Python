a7805082b5af0d73227382eb14f212ab32570060
Svetlana <Sea2100@mail.ru>
2019-04-09 10:26:55 +0300

    задание по модулям

---
+++ main.py
@@ -0,0 +1,16 @@
+# 3. Создайте модуль main.py. Из модулей, реализованных в заданиях 1 и 2, сделайте импорт в main.py всех функций.
+# # Вызовите каждую функцию в main.py и проверьте, что все работает как надо.
+# # Примечание: Попробуйте импортировать как весь модуль целиком (например из задачи 1),
+# # так и отдельные функции из модуля.
+import rand_from_list as rd
+import work_dir as dr
+
+num_list=[1,2,5,8,9,5,3]
+char_list=['s','d','f','g','h','dd','eee']
+emp_list=[]
+print(rd.rand_el(num_list))
+print(rd.rand_el(char_list))
+print(rd.rand_el(emp_list))
+
+dr.create_dir()
+#dr.del_dir()
